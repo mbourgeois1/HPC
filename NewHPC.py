be179745eb2e7a2c28c707cb3366bcdf08427bb4
mbourgeois1 <mbourgeois1@uco.edu>
2018-04-21 13:35:37 -0500

    Add files via upload

--- NewHPC.py
+++ NewHPC.py
@@ -15,6 +15,7 @@ def lu_det():
 
         for i in range((k + 1), int(n / m)):
             rightmulti(i,k,U_inv)
+
         for j in range((k+1), int(n/m)):
             leftmulti(k,j,L_inv)
         for i in range((k + 1), int(n / m)):
@@ -30,19 +31,21 @@ def lu_det():
 
 def rightmulti(i,k,U_inv):
     b = readSunMatrix(i,k)
-    b = b * U_inv
+    #print(b, U_inv)
+    b = np.dot(b, U_inv)
+    #print(b)
     writesubmatrix(i,k, b)
 
 def leftmulti(k,j,L_inv):
     b= readSunMatrix(k,j)
-    b = b * L_inv
+    b = np.dot(L_inv, b)
     writesubmatrix(k,j,b)
 
 def schur(i,j,k):
     b = readSunMatrix(i,j)
     c = readSunMatrix(i,k)
     d = readSunMatrix(k,j)
-    b = b - c*d
+    b = b - np.dot(c,d)
     writesubmatrix(i,j,b)
 
 def writesubmatrix(i,k,a):
@@ -57,13 +60,16 @@ def part2(i,j):
     log_determ = 0
     L, U = scipy.linalg.lu(a, permute_l = True)
     # print(P)
-    # print(L)
-    # print(U)
+    #print(L)
+    #print(U)
     L_inv = np.linalg.inv(L)
     U_inv = np.linalg.inv(U)
     determ = determ * (np.linalg.det(L) * np.linalg.det(U))
     log_determ += log(abs((np.linalg.det(L)))) + log(abs((np.linalg.det(U))))
+    #print(np.linalg.det(L),np.linalg.det(U))
+    #print(L_inv, U_inv, determ, log_determ)
     return L_inv, U_inv, determ, log_determ
+
 
 def readSunMatrix(i,j):
     str1 = "submatrix_" + str(i) + "_" + str(j) + ".npy"
@@ -72,10 +78,10 @@ def readSunMatrix(i,j):
 
 # end
 # Read the large/raw matrix and divide it into smaller block
-n = int(16)
-m = int(8)
+n = int(512)
+m = int(32)
 lst = []
-largematrix = open("m0016x0016.bin", "rb")
+largematrix = open("m0512x0512.bin", "rb")
 output = list(range(int(n / m)))
 submatrix_file = []
 for i in range(0, int(n / m)):
@@ -96,54 +102,6 @@ for i in range(0, int(n / m)):
         #submatrix.close()
 largematrix.close()
 
-# end of reading large matrix
 
+print (lu_det())
 
-# # Read the submatrix files and convert the submatrix into lists
-# determinant_megastuffed=[]
-# for i in range(0, int(n / m)):
-#     for j in range(0, int(n / m)):
-#
-#         # Reshape the list into matrix shape
-#         #megastuffed = np.reshape(megastuffed, (m,m))
-#
-#         # Calculate the determinant and log determinant of submatrix by using LU factorization
-#         determinant_megastuffed.append(lu_det())
-#         print(determinant_megastuffed)
-#         #print('The determinant and log determinant are' + str(determinant_megastuffed))
-
-#print(len(determinant_megastuffed))
-#determinant_megastuffed=np.reshape(determinant_megastuffed,(-1, int(sqrt(len(determinant_megastuffed)))))
-final_determinant=lu_det()
-print (final_determinant)
-
-
-
-
-
-
-
-        # submatrix_file = open("submatrix_0_0")
-        # blockmatrix=[]
-        # for i in range (0,(n/m)):
-        #     doublesub=submatrix_file.readline()
-        #     doublesub1=doublesub.strip('[')
-        #     doublesub1 = doublesub1.replace(']','')
-        #     doublesub1=doublesub1.split(",")
-        #     doublesub1 = list(map(float, doublesub1))
-        #     blockmatrix.append(doublesub1)
-
-        # blockmatrix=np.reshape(blockmatrix,(-1, int(len(blockmatrix))))
-        # #print(blockmatrix)
-
-        # b=lu_det(blockmatrix)
-        # print b
-
-
-
-
-
-
-        # print(main())
-        # a = np.arange(16).reshape(4, 4)
-        # print(np.linalg.det(a), log(abs(np.linalg.det(a))))
